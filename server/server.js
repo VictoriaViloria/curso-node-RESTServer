3589246e46bced7b58561cc4fafce12c94246ab8
viloria <viloria_f@yahoo.com.mx>
2019-07-16 15:06:33 -0500

    80. Peticiones HTTP - GET - PUT - POST - DELETE Y uso del body-parser

--- server/server.js
+++ server/server.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', function(req, res) {
-    // res.send('Hello World');
-    res.json('Hello World');
-})
+    res.json('holaaaa ');
+});
+//n GET se pueden actualizar registros
+app.get('/usuario', function(req, res) {
+    res.json('get usuario');
+});
+// POST crear nuevos registros 
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    res.json({
+        bodypersona: body
+    });
+});
+// PUT actualizar data, registros
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id: id
+    });
+});
+//DELETE
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario');
+});
 
 app.listen(3000, () => {
     console.log('escuchando puerto: ', 3000);
