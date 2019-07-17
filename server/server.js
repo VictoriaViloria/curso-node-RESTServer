ac3da9af864ab864e5d43adcea8bc95f65de727a
viloria <viloria_f@yahoo.com.mx>
2019-07-16 20:56:42 -0500

    84. Respaldo del RESTServer a GitHub

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
+require('./config/config');
+
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
+    // parse application/json
 app.use(bodyParser.json())
-
 app.get('/', function(req, res) {
     res.json('holaaaa ');
 });
@@ -18,9 +17,16 @@ app.get('/usuario', function(req, res) {
 // POST crear nuevos registros 
 app.post('/usuario', function(req, res) {
     let body = req.body;
-    res.json({
-        bodypersona: body
-    });
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre es necesario'
+        })
+    } else {
+        res.json({
+            bodypersona: body
+        });
+    }
 });
 // PUT actualizar data, registros
 app.put('/usuario/:id', function(req, res) {
@@ -33,7 +39,6 @@ app.put('/usuario/:id', function(req, res) {
 app.delete('/usuario', function(req, res) {
     res.json('delete usuario');
 });
-
-app.listen(3000, () => {
-    console.log('escuchando puerto: ', 3000);
+app.listen(process.env.PORT, () => {
+    console.log('escuchando puerto: ', process.env.PORT);
 });
