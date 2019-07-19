9d8aa5ee114b27ed401d84ba2af1233e8c2b97d6
viloria <viloria_f@yahoo.com.mx>
2019-07-19 13:52:14 -0500

    100.  GET: Obtener todos los usuarios de forma paginada limite desde

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-
 const app = express();
 const Usuario = require('../models/usuario');
 
@@ -11,7 +10,29 @@ app.get('/', function(req, res) {
 });
 //n GET se pueden actualizar registros
 app.get('/usuario', function(req, res) {
-    res.json('get usuario LOCAL !!!!!ji ji');
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 3;
+    limite = Number(limite);
+    Usuario.find({})
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                usuarios: usuarios
+            });
+        });
+    //.exec ejecutar ese find
+    //Usuario trae el Squema de usuario
+    // res.json('get usuario LOCAL !!!!!ji ji');
 });
 // POST crear nuevos registros 
 app.post('/usuario', function(req, res) {
