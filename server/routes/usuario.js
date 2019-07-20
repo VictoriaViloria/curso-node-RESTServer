362727dc43a6d6299baba271a4d06da9ec5aa984
viloria <viloria_f@yahoo.com.mx>
2019-07-19 23:22:04 -0500

    104. Delete: Marcar una eliminación en el mismo registro

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -16,7 +16,7 @@ app.get('/usuario', function(req, res) {
     let limite = req.query.limite || 3;
     limite = Number(limite);
     //Usuario.find({ google: true })
-    Usuario.find({}, ' nombre email role estado google img')
+    Usuario.find({ estado: true }, ' nombre email role estado google img')
         .skip(desde)
         .limit(limite)
         .exec((err, usuarios) => {
@@ -27,7 +27,7 @@ app.get('/usuario', function(req, res) {
                 });
             }
             //  Usuario.count({ google: true }, (err, conteo) => {
-            Usuario.count({}, (err, conteo) => {
+            Usuario.count({ estado: true }, (err, conteo) => {
                 res.json({
                     ok: true,
                     usuarios,
@@ -90,7 +90,12 @@ app.put('/usuario/:id', function(req, res) {
 //DELETE
 app.delete('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+    let cambiaEstado = {
+        estado: false
+    };
+
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
         //borramos el registro fisicamente
         if (err) {
             return res.status(400).json({
@@ -113,5 +118,30 @@ app.delete('/usuario/:id', function(req, res) {
     });
     //res.json('delete usuario');
 });
+// app.delete('/usuario/:id', function(req, res) {
+//     let id = req.params.id;
+//     Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+//         //borramos el registro fisicamente
+//         if (err) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 err
+//             })
+//         }
+//         if (usuarioBorrado === null) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 err: {
+//                     message: 'usuario no encontrado'
+//                 }
+//             });
+//         }
+//         res.json({
+//             ok: true,
+//             usuario: usuarioBorrado
+//         });
+//     });
+//     //res.json('delete usuario');
+// });
 
 module.exports = app;
