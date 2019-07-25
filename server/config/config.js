fcbc0ccffd2dcafb68d66a50c675c5d8cef10727
viloria <viloria_f@yahoo.com.mx>
2019-07-25 13:18:11 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://viloriamon:wXKY2YOOseWLkPGx@cluster0-kyxqi.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
